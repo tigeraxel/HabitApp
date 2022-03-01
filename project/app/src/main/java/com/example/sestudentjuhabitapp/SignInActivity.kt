2b333d09caf79fa9f747cf72bf1449a577b65e9c
Guzz00 <81687624+Guzz00@users.noreply.github.com>
2022-03-01 16:36:27 +0100

    Added clickListeners to login & sign Buttons

--- project/app/src/main/java/com/example/sestudentjuhabitapp/SignInActivity.kt
+++ project/app/src/main/java/com/example/sestudentjuhabitapp/SignInActivity.kt
@@ -1,11 +1,21 @@
 package com.example.sestudentjuhabitapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+
+
+        val btnCreateAccount = findViewById<Button>(R.id.btnCreateAccount)
+        btnCreateAccount.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }

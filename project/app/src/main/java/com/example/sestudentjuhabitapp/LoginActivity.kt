eda059d1d089fe6625d7eb3c760273ab70c3467c
Axel <axeltigerberg@gmail.com>
2022-03-06 13:50:16 +0100

    Login & Register now implemented with FireBase Authentication.

--- project/app/src/main/java/com/example/sestudentjuhabitapp/LoginActivity.kt
+++ project/app/src/main/java/com/example/sestudentjuhabitapp/LoginActivity.kt
@@ -1,22 +1,87 @@
 package com.example.sestudentjuhabitapp
 
+
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import android.widget.Toast
+
+import android.content.ContentValues.TAG
+import android.util.Log
+import android.widget.EditText
+
 
 class LoginActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        mAuth = FirebaseAuth.getInstance();
 
 
-        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val btnLogin = findViewById<Button>(R.id.btnCreate)
+        val btnCreateAccount = findViewById<Button>(R.id.createAccountButton)
+        val loginEmail = findViewById<EditText>(R.id.loginUsernameEditText)
+        val loginPassword = findViewById<EditText>(R.id.loginPassswordEditText)
+
+        btnCreateAccount.setOnClickListener {
+            startActivity(Intent(this, createAccountActivity::class.java))
+        }
+        btnLogin.setOnClickListener {
+
+            mAuth!!.signInWithEmailAndPassword(
+                loginEmail.getText().toString(),
+                loginPassword.getText().toString()
+            )
+                .addOnCompleteListener(
+                    this
+                ) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val userID = mAuth!!.currentUser!!.providerId
+                        startActivity(
+                            Intent(this, MainActivity::class.java).putExtra(
+                                "userID",
+                                userID
+                            )
+                        )
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            this@LoginActivity, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                        updateUI(null)
+                    }
+
+                    // ...
+                }
+        }
+
+        //val btnLogin = findViewById<Button>(R.id.btnLogin)
 
         //Login in the user, starts MainActivity when clicked
-        btnLogin.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = mAuth!!.getCurrentUser();
+        updateUI(currentUser)
+
+
+    }
+
+    fun updateUI(user: FirebaseUser?) {
+
+        return
     }
 }
---
+++ project/app/src/main/java/com/example/sestudentjuhabitapp/createAccountActivity.kt
@@ -0,0 +1,59 @@
+package com.example.sestudentjuhabitapp
+
+import android.content.ContentValues
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class createAccountActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+        mAuth = FirebaseAuth.getInstance();
+
+        val btnCreate = findViewById<Button>(R.id.btnCreate)
+        val btnLoginToAccount = findViewById<Button>(R.id.btnLoginToAccount)
+        val createEmail = findViewById<EditText>(R.id.createUsernameEditText)
+        val createPassword = findViewById<EditText>(R.id.createPassswordEditText)
+
+        btnLoginToAccount.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        btnCreate.setOnClickListener {
+            mAuth!!.createUserWithEmailAndPassword(
+                createEmail.getText().toString(),
+                createPassword.getText().toString()
+            )
+                .addOnCompleteListener(
+                    this
+                ) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val userID = mAuth!!.currentUser!!.providerId
+                        startActivity(
+                            Intent(this, MainActivity::class.java).putExtra(
+                                "userID",
+                                userID
+                            )
+                        )
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(ContentValues.TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            this@createAccountActivity, "Authentication failed.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+
+                }
+        }
+    }
+}
